32baf41fa8395bc8e5e84246c6e1f98e40284727
Tanya Alexa Delgado Olguín <tanyaalexadelgadoolguin@Tanyas-MacBook-Air.local>
2024-02-23 20:50:31 -0600

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-import {defineConfig } from 'vite'
-import react from @vitejs/plugin-react 
-import plugin from 'tailwindcss'
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
-  },https://<Alexa-Delgado>.github.io/React-Rick-and-Morty
+  },
   plugins: [],
 }
 
- 
